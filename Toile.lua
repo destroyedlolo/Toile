16e008f2383c28e449fee150281ef1939595c906
destroyedlolo <l_faillie@yahoo.com>
2017-03-18 12:31:33 +0100

    Migration from HDB finished

---
+++ Toile.lua
@@ -0,0 +1,34 @@
+TOILE_VERSION=0.0001
+
+-- modules helpers
+lfs = require "lfs" -- LuaFileSystem
+
+function loaddir(path, dir )
+	local t={}
+
+	for f in lfs.dir(path..dir) do
+		local attr = lfs.attributes( path..dir ..'/'.. f )
+		local found, len, res = f:find("^(.*)%.[^%.]*$")
+		if found and attr.mode == 'file' and res:sub(1,1) ~= '.' and f:match("^.+(%..+)$") ~= '.md' then
+			table.insert( t, res )
+		end
+	end
+
+	table.sort(t)
+
+	for _,res in ipairs( t ) do
+		require(dir ..'/'.. res)
+		SelLog.log("*L* " .. dir ..'/'.. res .. ' loaded')
+	end
+end
+
+-- load modules
+local info = debug.getinfo(1,'S');
+local whereiam = string.match(info.source, "@(.-)([^\\/]-%.?([^%.\\/]*))$")
+
+loaddir(whereiam, 'Supports')
+animTimer = bipTimer(.25)	-- Animation timer
+
+loaddir(whereiam, 'Inputs')
+loaddir(whereiam, 'GUI')
+
