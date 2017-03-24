88bcf0fe761981003aeec54cd90b3d8a66571eab
destroyedlolo <l_faillie@yahoo.com>
2017-03-24 19:32:19 +0100

    externalize Align

--- GUI/30_Field.lua
+++ GUI/30_Field.lua
@@ -1,8 +1,4 @@
 -- Field to display a value
-ALIGN_LEFT = -1
-ALIGN_CENTER = 0
-ALIGN_RIGHT = 1
-ALIGN_FRIGHT = 2	-- x is the right limit and the real position depend on width
 
 function Field(
 	psrf,	-- mother surface
