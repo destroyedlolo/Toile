26a45000b9e6faedc251d35d32fd7026a019634e
destroyedlolo <l_faillie@yahoo.com>
2017-04-07 22:21:43 +0200

    Secure if some fields are missing

--- Inputs/20_Weather3H.lua
+++ Inputs/20_Weather3H.lua
@@ -16,12 +16,24 @@ function Weather3H(
 
 	local self = MQTTinput( city..'3H'..time, topic ..'/'.. city ..'/'.. time ..'/time')
 	local icn = MQTTinput( city..'3H'..time..'acode', topic ..'/'.. city ..'/'.. time ..'/weather/acode')
-	local desc = MQTTDisplay( city..'3H'..time..'desc', topic ..'/'.. city ..'/'.. time ..'/weather/description', dsp.desc)
-	local temp = MQTTDisplay( city..'3H'..time..'temp', topic ..'/'.. city ..'/'.. time ..'/temperature', dsp.temp )
-	local wspd = MQTTDisplay( city..'3H'..time..'wspd', topic ..'/'.. city ..'/'.. time ..'/wind/speed', dsp.windspeed, { align = ALIGN_RIGHT } )
-	local wdir = MQTTDisplay( city..'3H'..time..'wdir', topic ..'/'.. city ..'/'.. time ..'/wind/direction', dsp.windd)
-	local clouds = MQTTDisplay( city..'3H'..time..'clouds', topic ..'/'.. city ..'/'.. time ..'/clouds', dsp.clouds)
-	local humidity = MQTTDisplay( city..'3H'..time..'humidity', topic ..'/'.. city ..'/'.. time ..'/humidity', dsp.humidity)
+	if dsp.desc then
+		MQTTDisplay( city..'3H'..time..'desc', topic ..'/'.. city ..'/'.. time ..'/weather/description', dsp.desc)
+	end
+	if dsp.temp then
+		MQTTDisplay( city..'3H'..time..'temp', topic ..'/'.. city ..'/'.. time ..'/temperature', dsp.temp )
+	end
+	if dsp.windspeed then
+		MQTTDisplay( city..'3H'..time..'wspd', topic ..'/'.. city ..'/'.. time ..'/wind/speed', dsp.windspeed, { align = ALIGN_RIGHT } )
+	end
+	if dsp.windd then
+		MQTTDisplay( city..'3H'..time..'wdir', topic ..'/'.. city ..'/'.. time ..'/wind/direction', dsp.windd)
+	end
+	if dsp.windd then
+		MQTTDisplay( city..'3H'..time..'clouds', topic ..'/'.. city ..'/'.. time ..'/clouds', dsp.clouds)
+	end
+	if dsp.humidity then
+		MQTTDisplay( city..'3H'..time..'humidity', topic ..'/'.. city ..'/'.. time ..'/humidity', dsp.humidity)
+	end
 
 	dsp.setName( city..'3H'..time )
 	
