c4c2ca5c89b1472cf47d2dd241e5f6c996458a45
destroyedlolo <l_faillie@yahoo.com>
2017-07-01 18:29:45 +0200

    Topic2Userdata working

--- Inputs/20_MQTTLog.lua
+++ Inputs/20_MQTTLog.lua
@@ -6,7 +6,6 @@ function _MQTTLog(aname, atpc,	-- 1st topic
 --[[ known options :
 --]]
 	local fifo = SelFIFO.Create(aname)
-	local tpu = {}	-- Link b/w topic and userdata
 
 	local pushmsg	-- early defintion for callback function
 	local self = MQTTinput(aname, atpc, 
@@ -14,12 +13,16 @@ function _MQTTLog(aname, atpc,	-- 1st topic
 	opts)
 
 	function self.fifoname()	-- function to be redefined to declare topic name
-		return nil -- name of this fifo, can't be a variable
+		return nil -- name of this fifo
 	end
+
+	function self.topic2udata()	-- function to be redefined to lookup userdata from a topic
+		return nil	-- lookup table to get user data for a topic
+	end	
 
 	pushmsg = function (topic, data)
 		local fifo = SelFIFO.Find(self.fifoname())
-		SelFIFO.Push2FIFO(fifo, data, tpu[topic])
+		SelFIFO.Push2FIFO(fifo, data, self.topic2udata(topic))
 	end
 
 	local function display()
@@ -30,7 +33,6 @@ function _MQTTLog(aname, atpc,	-- 1st topic
 		end
 	end
 
-	tpu[atpc] = 0
 	self.TaskOnceAdd( display )
 
 	return self
@@ -40,12 +42,23 @@ function MQTTLog( aname, atpc,	-- 1st topic
 	srf, -- where to display
 	opts )
 --[[ known options :
+		udata = userdata for provided atpc (default : 0)
 --]]
+	if not opts then
+		opts = {}
+	end
+
+	local t2u = {}	-- Lookup table b/w topic and userdata
+	t2u[ atpc ] = opts.udata or 0
 
 	local self = _MQTTLog(aname, atpc, srf, opts )
 
 	function self.fifoname()
 		return aname
+	end
+
+	function self.topic2udata(t)
+		return t2u[t]
 	end
 
 	return self
