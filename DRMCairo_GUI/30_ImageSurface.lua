4bf468b74ed9e863413eb3fcbdaf6fb104fa2d8d
destroyedlolo <l_faillie@yahoo.com>
2020-08-19 00:56:21 +0200

    Add autoscale to ImageSurface

--- DRMCairo_GUI/30_ImageSurface.lua
+++ DRMCairo_GUI/30_ImageSurface.lua
@@ -8,6 +8,7 @@ function ImageSurface(
 )
 --[[ known options :
 	transparency : call Clear() of the parent surface at Update()
+	autoscale : scale the image to fit surface
 --]]
 	if not opts then
 		opts = {}
@@ -19,8 +20,22 @@ function ImageSurface(
 		if opts.transparency then
 			psrf.Clear( {sx,sy, sw,sh} )
 		end
+
+		if opts.autoscale then
+				-- Scale calculation
+			local x,y = img:GetSize()
+			x = math.min(sw/x, sh/y)
+
+			self.get():SaveContext()
+			self.get():Scale(x,x)
+		end
+
 		self.get():Blit( img, 0,0 )
 		self.Refresh()
+
+		if opts.autoscale then
+			self.get():RestoreContext()
+		end
 	end
 
 	return self
